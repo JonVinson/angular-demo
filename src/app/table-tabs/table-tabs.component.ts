b724b7f443f7f882db92840215b570f5cca0c062
Jon Vinson <jdv@jdvinson.com>
2025-09-17 14:34:13 -0500

    INV-103 Added products table to tabs

--- src/app/table-tabs/table-tabs.component.ts
+++ src/app/table-tabs/table-tabs.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTab } from '@angular/material/tabs';
 import { TableTwoComponent } from "../table-two/table-two.component";
+import { ProductTableComponent } from "../product-table/product-table.component";
 
 @Component({
   selector: 'app-table-tabs',
-  imports: [MatTabsModule, TableTwoComponent],
+  imports: [MatTabsModule, TableTwoComponent, ProductTableComponent],
   templateUrl: './table-tabs.component.html',
   styleUrl: './table-tabs.component.scss'
 })
